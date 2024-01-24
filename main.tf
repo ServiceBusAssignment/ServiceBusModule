bc471a1ae9617dba9c072d69b2320a130c9971cf
mleisa <melcafidan@gmail.com>
2024-01-24 14:10:44 +0100

    Flatten folder structure

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ provider "azurerm" {
 }
 
 module "resource-group" {
-  source = "./resource-group"
+  source = "resource-group"
   resource_group_location = var.resouce_group_location
   resource_group_name = var.resource_group_name
 }
 
 module "servicebus-namespace" {
-  source = "./servicebus-namespace"
+  source = "servicebus-namespace"
   namespace_name = var.namespace_name
   resource_group_name = module.resource-group.resource_group_name
   resource_group_location = module.resource-group.resource_group_location
@@ -27,21 +27,21 @@ module "servicebus-namespace" {
 }
 
 module "servicebus-queue" {
-  source = "./servicebus-queue"
+  source = "servicebus-queue"
   queue_name = var.queue_name
   namespace_id = module.servicebus-namespace.namespace_id
   dead_lettering_enabled = var.dead_lettering_enabled
 }
 
 module "action-group" {
-  source = "./action-group"
+  source = "action-group"
   resource_group_name = module.resource-group.resource_group_name
   action_group_name = var.action_group_name
   short_name = var.action_group_short_name
 }
 
 module "metric-alert" {
-  source = "./metric-alert"
+  source = "metric-alert"
   action_group_id = module.action-group.action_group_id
   alert_name = var.alert_name
   message_threshold = var.alert_message_threshold
