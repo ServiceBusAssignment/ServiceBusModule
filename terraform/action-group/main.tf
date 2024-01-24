26948129b154f49db3bde84d564e1d0c13f08752
mleisa <melcafidan@gmail.com>
2024-01-24 12:17:25 +0100

    Action group variable

--- terraform/action-group/main.tf
+++ terraform/action-group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_action_group" "monitor-action-group" {
-  name                = "MessageAlertsAction"
+  name                = var.action_group_name
   resource_group_name = var.resource_group_name
   short_name          = var.short_name
 }
